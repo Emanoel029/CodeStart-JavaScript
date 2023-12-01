43f193bcef8a0284280b22172497398414148838
Emanoel Timbó <emanoelpontes8@gmail.com>
2023-12-01 18:35:38 -0300

    Aprendendo a selecionar elementos

---
+++ script.js
@@ -0,0 +1,16 @@
+const titulo = document.getElementById("titulo")
+console.log(titulo)
+
+// const tituloTag = document.getElementsByTagName("h1")
+// console.log(tituloTag)
+
+const classTitle = document.getElementsByClassName("titulo-javascript")
+console.log(classTitle)
+
+
+const subTitle = document.querySelectorAll("h2")
+console.log(subTitle)
+
+const arryElements = document.querySelectorAll(".lista ul li")
+console.log(arryElements)
+
